f9bfb39891702b94aafa9153f9b85df164bbbd56
Ankit Sharma <asharma@dainsta.com>
2023-12-15 11:12:03 +0530

    first commit

---
+++ src/fetchApp.jsx
@@ -0,0 +1,8 @@
+import React, { useState } from 'react'
+
+export default function FetchApp() {
+    const [currentPrice, setCurrentPrice] = useState(0);
+
+            
+  return  <div>fetchApp</div>
+}
